8394caf91a284f60e30bf47c64fd0c935d1292ab
kayeon kang <50822293+goriver@users.noreply.github.com>
2020-09-16 19:30:35 +0900

    종현씨 코드

--- leejh/fbprophet_main.py
+++ leejh/fbprophet_main.py
@@ -1,5 +1,6 @@
-from using_fbprophet import PredictByProphet
+import using_fbprophet 
 import datetime
+import pandas as pd
 # 매일 저녁에 서버 다운 or 점검 시간에 수행
 
 
@@ -10,9 +11,60 @@ import datetime
 # 카카오,삼성,네이버 로 fix
 if __name__ == "__main__":
     corporation = ['034730', '003550','030200','035720','005930','035420']
-    fbpro = PredictByProphet.main()
+    # fbpro = PredictByProphet.main()
     today = datetime.datetime.today() 
     date = today.strftime('%Y%m%d') 
+    date = int(date)
+    pd.set_option('mode.chained_assignment',  None)
+    # using_fbprophet.
     for i in corporation:
-        fbpro(i, date)
+        temp, what_day = using_fbprophet.make_train_data(i, date)
+
+        df = pd.DataFrame(columns=['ds', 'y'])
+
+        df['ds'] = temp['DateTime']  # 훈련용 데이터프레임 생성
+
+        df['y'] = temp['체결가']  # 앞으로 쓸 y값 지정
+
+        df['y'].plot()
+        plt.savefig('weekend.png', dpi=400)
+
+
+        m = Prophet(changepoint_range=0.1).fit(df)
+        future = m.make_future_dataframe(periods=1500, freq='min')
+
+        future2 = future[(future['ds'].dt.day == what_day)]
+
+        am = future2[ (future2['ds'].dt.hour >= 9) & (future2['ds'].dt.hour < 12) ]
+        pm = future2[ (future2['ds'].dt.hour >= 12) & (future2['ds'].dt.hour < 16) ]
+
+
+        am_pred = m.predict(am)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=am_pred['ds'],y=am_pred['yhat'],
+                    mode='lines+markers', name='실제값'))
+        fig.update_layout(title='<b>해당 요일 오전의 예측 주가</b>')
+        pio.write_html(fig, "C:/Users/A0501660/djangogirls/djangogirls/django/templates/blog/leejh/AM_{}.html".format(index_code), config=None, auto_play=True, include_plotlyjs=True, include_mathjax=False, post_script=None, full_html=True, animation_opts=None, validate=True, default_width='100%', default_height='100%', auto_open=False)
+
+
+
+        pm_pred = m.predict(pm)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=pm_pred['ds'],y=pm_pred['yhat'],
+                    mode='lines+markers', name='실제값'))
+        fig.update_layout(title='<b>해당 요일 오후의 예측 주가</b>')
+        pio.write_html(fig, "C:/Users/A0501660/djangogirls/djangogirls/django/templates/blog/leejh/PM_{}.html".format(index_code), config=None, auto_play=True, include_plotlyjs=True, include_mathjax=False, post_script=None, full_html=True, animation_opts=None, validate=True, default_width='100%', default_height='100%', auto_open=False)
+
+
+
+        print("해당 종목의 오전 추천 매수가는 ", am_pred['yhat'].min(), "입니다.")
+        print("해당 종목의 오전 추천 매도가는 ", am_pred['yhat'].max(), "입니다.")
+
+        print("해당 종목의 오후 추천 매수가는 ", pm_pred['yhat'].min(), "입니다.")
+        print("해당 종목의 오후 추천 매도가는 ", pm_pred['yhat'].max(), "입니다.")
+
+
+
+
+
 
--- leejh/using_fbprophet.py
+++ leejh/using_fbprophet.py
@@ -10,172 +10,172 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from plotly.subplots import make_subplots
 
-class PredictByProphet :
-    def make_train_data(index_code, want_date):
+
+def make_train_data(index_code, want_date):
+    
+    # index_code = '000100'  # 종목코드 넣어줌
+    # want_date = 20200915   # 예측하고 싶은 요일을 넣어줌
+
+    input_date = want_date-7
+
+    wd = str(want_date)
+
+    day = wd[6:8]
+
+    int_day = int(day)
+
+    frames = []
+
+    for i in range(7):
+        url='https://finance.naver.com/item/sise_time.nhn?code='+ index_code +'&thistime='+ str(input_date) + '16'
         
-        # index_code = '000100'  # 종목코드 넣어줌
-        # want_date = 20200915   # 예측하고 싶은 요일을 넣어줌
-
-        input_date = want_date-7
-
-        wd = str(want_date)
-
-        day = wd[6:8]
-
-        int_day = int(day)
-
-        frames = []
-
-        for i in range(7):
-            url='https://finance.naver.com/item/sise_time.nhn?code='+ index_code +'&thistime='+ str(input_date) + '16'
-            
-            resp = requests.get(url)
-            html = BeautifulSoup(resp.content, 'html.parser')
-            
-            holiday = html.find("td",{"class":"pgRR"})
-            
-            if holiday is None:  #주말은 제외
-                
-                input_date+=1
-                continue
-                
-            else:
-                
-                last_page = html.find("td",{"class":"pgRR"}).find('a')['href'].split('&')[2].split('=')[1]
-                lastpage = int(last_page)
-                df=pd.DataFrame()
-                
-                for page in range(1, lastpage+1):
-                    pg_url = '{url}&page={page}'.format(url=url, page=page)
-                    df = df.append(pd.read_html(pg_url, header=0)[0], ignore_index=True)
-                    
-                rdf = df.dropna(axis=0)
-                
-                rdf['date'] = input_date
-                
-                rdf['date'] = rdf['date'].astype(str)
-                rdf["date"] = rdf["date"].str[0:4] + "-" + rdf["date"].str[4:6] + "-" + rdf["date"].str[6:8]
-                rdf["date"] = rdf["date"].astype('datetime64[ns]')
-
-                rdf['DateTime'] = pd.to_datetime(rdf.date.dt.strftime("%Y-%m-%d") + " " + rdf.체결시각)
-
-                frames.append(rdf)
-                input_date+=1
-
-        frames.reverse()
-
-        final_frame = pd.concat(frames, ignore_index=True)
-
-        data = final_frame[::-1].reset_index(drop=True)
-
-        return data, int_day
-
-
-    def make_test_data(index_code, want_date):
+        resp = requests.get(url)
+        html = BeautifulSoup(resp.content, 'html.parser')
         
-        # index_code = '000100'  # 종목코드 넣어줌
-        # want_date = 20200915   # 예측하고 싶은 요일을 넣어줌
-
-        input_date = want_date
-
-        wd = str(want_date)
-
-        day = wd[6:8]
-
-        int_day = int(day)
-
-        frames = []
-
-        for i in range(1):
-            url='https://finance.naver.com/item/sise_time.nhn?code='+ index_code +'&thistime='+ str(input_date) + '16'
+        holiday = html.find("td",{"class":"pgRR"})
+        
+        if holiday is None:  #주말은 제외
             
-            resp = requests.get(url)
-            html = BeautifulSoup(resp.content, 'html.parser')
+            input_date+=1
+            continue
             
-            holiday = html.find("td",{"class":"pgRR"})
+        else:
             
-            if holiday is None:  #주말은 제외
+            last_page = html.find("td",{"class":"pgRR"}).find('a')['href'].split('&')[2].split('=')[1]
+            lastpage = int(last_page)
+            df=pd.DataFrame()
+            
+            for page in range(1, lastpage+1):
+                pg_url = '{url}&page={page}'.format(url=url, page=page)
+                df = df.append(pd.read_html(pg_url, header=0)[0], ignore_index=True)
                 
-                input_date+=1
-                continue
+            rdf = df.dropna(axis=0)
+            
+            rdf['date'] = input_date
+            
+            rdf['date'] = rdf['date'].astype(str)
+            rdf["date"] = rdf["date"].str[0:4] + "-" + rdf["date"].str[4:6] + "-" + rdf["date"].str[6:8]
+            rdf["date"] = rdf["date"].astype('datetime64[ns]')
+
+            rdf['DateTime'] = pd.to_datetime(rdf.date.dt.strftime("%Y-%m-%d") + " " + rdf.체결시각)
+
+            frames.append(rdf)
+            input_date+=1
+
+    frames.reverse()
+
+    final_frame = pd.concat(frames, ignore_index=True)
+
+    data = final_frame[::-1].reset_index(drop=True)
+
+    return data, int_day
+
+
+def make_test_data(index_code, want_date):
+    
+    # index_code = '000100'  # 종목코드 넣어줌
+    # want_date = 20200915   # 예측하고 싶은 요일을 넣어줌
+
+    input_date = want_date
+
+    wd = str(want_date)
+
+    day = wd[6:8]
+
+    int_day = int(day)
+
+    frames = []
+
+    for i in range(1):
+        url='https://finance.naver.com/item/sise_time.nhn?code='+ index_code +'&thistime='+ str(input_date) + '16'
+        
+        resp = requests.get(url)
+        html = BeautifulSoup(resp.content, 'html.parser')
+        
+        holiday = html.find("td",{"class":"pgRR"})
+        
+        if holiday is None:  #주말은 제외
+            
+            input_date+=1
+            continue
+            
+        else:
+            
+            last_page = html.find("td",{"class":"pgRR"}).find('a')['href'].split('&')[2].split('=')[1]
+            lastpage = int(last_page)
+            df=pd.DataFrame()
+            
+            for page in range(1, lastpage+1):
+                pg_url = '{url}&page={page}'.format(url=url, page=page)
+                df = df.append(pd.read_html(pg_url, header=0)[0], ignore_index=True)
                 
-            else:
-                
-                last_page = html.find("td",{"class":"pgRR"}).find('a')['href'].split('&')[2].split('=')[1]
-                lastpage = int(last_page)
-                df=pd.DataFrame()
-                
-                for page in range(1, lastpage+1):
-                    pg_url = '{url}&page={page}'.format(url=url, page=page)
-                    df = df.append(pd.read_html(pg_url, header=0)[0], ignore_index=True)
-                    
-                rdf = df.dropna(axis=0)
-                
-                rdf['date'] = input_date
-                
-                rdf['date'] = rdf['date'].astype(str)
-                rdf["date"] = rdf["date"].str[0:4] + "-" + rdf["date"].str[4:6] + "-" + rdf["date"].str[6:8]
-                rdf["date"] = rdf["date"].astype('datetime64[ns]')
+            rdf = df.dropna(axis=0)
+            
+            rdf['date'] = input_date
+            
+            rdf['date'] = rdf['date'].astype(str)
+            rdf["date"] = rdf["date"].str[0:4] + "-" + rdf["date"].str[4:6] + "-" + rdf["date"].str[6:8]
+            rdf["date"] = rdf["date"].astype('datetime64[ns]')
 
-                rdf['DateTime'] = pd.to_datetime(rdf.date.dt.strftime("%Y-%m-%d") + " " + rdf.체결시각)
+            rdf['DateTime'] = pd.to_datetime(rdf.date.dt.strftime("%Y-%m-%d") + " " + rdf.체결시각)
 
-                frames.append(rdf)
-                input_date+=1
+            frames.append(rdf)
+            input_date+=1
 
-        frames.reverse()
+    frames.reverse()
 
-        final_frame = pd.concat(frames, ignore_index=True)
+    final_frame = pd.concat(frames, ignore_index=True)
 
-        data = final_frame[::-1].reset_index(drop=True)
+    data = final_frame[::-1].reset_index(drop=True)
 
-        return data, int_day
+    return data, int_day
 
-    def main(code, date):
-        # pass
-        temp, what_day = make_train_data(code, date)
+# def main(code, date):
+    # pass
+    # temp, what_day = make_train_data(code, date)
 
-        df = pd.DataFrame(columns=['ds', 'y'])
+    # df = pd.DataFrame(columns=['ds', 'y'])
 
-        df['ds'] = temp['DateTime']  # 훈련용 데이터프레임 생성
+    # df['ds'] = temp['DateTime']  # 훈련용 데이터프레임 생성
 
-        df['y'] = temp['체결가']  # 앞으로 쓸 y값 지정
+    # df['y'] = temp['체결가']  # 앞으로 쓸 y값 지정
 
-        df['y'].plot()
-        plt.savefig('weekend.png', dpi=400)
+    # df['y'].plot()
+    # plt.savefig('weekend.png', dpi=400)
 
 
-        m = Prophet(changepoint_range=0.1).fit(df)
-        future = m.make_future_dataframe(periods=1500, freq='min')
+    # m = Prophet(changepoint_range=0.1).fit(df)
+    # future = m.make_future_dataframe(periods=1500, freq='min')
 
-        future2 = future[(future['ds'].dt.day == what_day)]
+    # future2 = future[(future['ds'].dt.day == what_day)]
 
-        am = future2[ (future2['ds'].dt.hour >= 9) & (future2['ds'].dt.hour < 12) ]
-        pm = future2[ (future2['ds'].dt.hour >= 12) & (future2['ds'].dt.hour < 16) ]
+    # am = future2[ (future2['ds'].dt.hour >= 9) & (future2['ds'].dt.hour < 12) ]
+    # pm = future2[ (future2['ds'].dt.hour >= 12) & (future2['ds'].dt.hour < 16) ]
 
 
-        am_pred = m.predict(am)
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=am_pred['ds'],y=am_pred['yhat'],
-                    mode='lines+markers', name='실제값'))
-        fig.update_layout(title='<b>해당 요일 오전의 예측 주가</b>')
-        pio.write_html(fig, "C:/Users/A0501660/djangogirls/djangogirls/django/templates/blog/leejh/AM_{}.html".format(index_code), config=None, auto_play=True, include_plotlyjs=True, include_mathjax=False, post_script=None, full_html=True, animation_opts=None, validate=True, default_width='100%', default_height='100%', auto_open=False)
+    # am_pred = m.predict(am)
+    # fig = go.Figure()
+    # fig.add_trace(go.Scatter(x=am_pred['ds'],y=am_pred['yhat'],
+    #             mode='lines+markers', name='실제값'))
+    # fig.update_layout(title='<b>해당 요일 오전의 예측 주가</b>')
+    # pio.write_html(fig, "C:/Users/A0501660/djangogirls/djangogirls/django/templates/blog/leejh/AM_{}.html".format(index_code), config=None, auto_play=True, include_plotlyjs=True, include_mathjax=False, post_script=None, full_html=True, animation_opts=None, validate=True, default_width='100%', default_height='100%', auto_open=False)
 
 
 
-        pm_pred = m.predict(pm)
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=pm_pred['ds'],y=pm_pred['yhat'],
-                    mode='lines+markers', name='실제값'))
-        fig.update_layout(title='<b>해당 요일 오후의 예측 주가</b>')
-        pio.write_html(fig, "C:/Users/A0501660/djangogirls/djangogirls/django/templates/blog/leejh/PM_{}.html".format(index_code), config=None, auto_play=True, include_plotlyjs=True, include_mathjax=False, post_script=None, full_html=True, animation_opts=None, validate=True, default_width='100%', default_height='100%', auto_open=False)
+    # pm_pred = m.predict(pm)
+    # fig = go.Figure()
+    # fig.add_trace(go.Scatter(x=pm_pred['ds'],y=pm_pred['yhat'],
+    #             mode='lines+markers', name='실제값'))
+    # fig.update_layout(title='<b>해당 요일 오후의 예측 주가</b>')
+    # pio.write_html(fig, "C:/Users/A0501660/djangogirls/djangogirls/django/templates/blog/leejh/PM_{}.html".format(index_code), config=None, auto_play=True, include_plotlyjs=True, include_mathjax=False, post_script=None, full_html=True, animation_opts=None, validate=True, default_width='100%', default_height='100%', auto_open=False)
 
 
 
-        print("해당 종목의 오전 추천 매수가는 ", am_pred['yhat'].min(), "입니다.")
-        print("해당 종목의 오전 추천 매도가는 ", am_pred['yhat'].max(), "입니다.")
+    # print("해당 종목의 오전 추천 매수가는 ", am_pred['yhat'].min(), "입니다.")
+    # print("해당 종목의 오전 추천 매도가는 ", am_pred['yhat'].max(), "입니다.")
 
-        print("해당 종목의 오후 추천 매수가는 ", pm_pred['yhat'].min(), "입니다.")
-        print("해당 종목의 오후 추천 매도가는 ", pm_pred['yhat'].max(), "입니다.")
+    # print("해당 종목의 오후 추천 매수가는 ", pm_pred['yhat'].min(), "입니다.")
+    # print("해당 종목의 오후 추천 매도가는 ", pm_pred['yhat'].max(), "입니다.")
 
 
 
